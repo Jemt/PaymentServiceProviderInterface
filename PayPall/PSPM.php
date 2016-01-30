075a92a636d068b92906d7e4f6345df287aceaf5
Jimmy Thomsen <jimmy@codemagic.dk>
2016-01-30 14:35:19 +0100

    Added payment providers
    Added PSPMs for popular payment providers

---
+++ PayPall/PSPM.php
@@ -0,0 +1,9 @@
+<?php
+
+class PSPM implements PSPI
+{
+	// Missing implementation.
+	// Interface definition: https://github.com/Jemt/StandardizedPaymentProviderInterface/blob/master/PSPInterface.php
+}
+
+?>
